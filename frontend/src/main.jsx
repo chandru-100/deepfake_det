a3204c4ec560b31565e15f4b5ed35610523d9e40
Chandrakanth D U <chandrakanthndu@Chandrakanths-MacBook-Air.local>
2026-05-23 13:12:59 +0530

    Secure Google Client ID in frontend .env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = '457833117105-3l1dmlkda1b4pfrq6atvrf68203a988j.apps.googleusercontent.com';
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
